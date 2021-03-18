3c901e81b30615a72cd44c92a10c2551517d7174
Jerry <GerardoGonzalezCode@gmail.com>
2021-03-18 00:07:30 -0600

    examen1-cloud v1.3.0

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const config = require('./config.js');
 const express= require('express');
+const cors= require('cors');
 const app=express();
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.post('/sentiment',(req,res) => {
 
@@ -29,7 +31,7 @@ app.post('/sentiment',(req,res) => {
     .then(toneAnalysis => {
       console.log(JSON.stringify(toneAnalysis, null, 2)); 
       console.log({"sentiment":toneAnalysis.result.document_tone.tones[0].tone_id });
-      res.send(toneAnalysis.result.document_tone.tones[0].tone_id );
+      res.send(JSON.stringify(toneAnalysis.result.document_tone.tones));
     })
     .catch(err => {
       console.log('error:', err);
