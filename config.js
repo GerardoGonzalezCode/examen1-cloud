277f37ede6895b2d234bb899482f50f67913e5cf
Jerry <GerardoGonzalezCode@gmail.com>
2021-03-17 19:50:42 -0600

    examen1-cloud v1.1.2

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
-    URL_TONE: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/4a9f6af1-d939-4b1d-b02a-82bccc62f6a1',
-    APIKEY_TONE: 'PD6fELS--JVx0cVnsa-EzF_8qQEPJMpk6GBa4owtyoBw',
+    URL_TONE: process.env.API_URL,
+    APIKEY_TONE: process.env.API_KEY,
     PORT:  3000
 }
